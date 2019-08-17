269d6c0524daf114b42edae15d8b6d75ed3c6ed9
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2019-08-17 11:05:14 +0200

    Adhere to eslint

--- App.js
+++ App.js
@@ -1,111 +1,102 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * License plate picker App
  *
  * @format
  * @flow
  */
 
-import React, {Component, Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
 
-import { WheelPicker } from 'react-native-wheel-picker-android'
+import {WheelPicker} from 'react-native-wheel-picker-android';
 
 import licensePlateData from './python/license-plate-data.json';
 
-const letterList = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+const letterList = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column'
+    flexDirection: 'column',
   },
   intro: {
     flex: 2,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
   pickerContainer: {
     flex: 1,
     backgroundColor: '#ddd',
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   results: {
     flex: 3,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
   picker: {
-    width: '30%'
-  }
+    width: '30%',
+  },
 });
 
 export default class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      result: 'Spin the wheels!'
-    }
+      result: 'Spin the wheels!',
+    };
 
-    this.selectedLetters = []
-
-    this.selectedLetter1 = ''
-    this.selectedLetter2 = ''
-    this.selectedLetter3 = ''
+    this.selectedLetters = [];
   }
 
-  onItemSelected = (selectedItem, index) => {
-    this.selectedLetters[index] = letterList[selectedItem]
-    console.log(this.selectedLetters)
-    this.updateResult()
+  onItemSelected(selectedItem, index) {
+    this.selectedLetters[index] = letterList[selectedItem];
+    console.log(this.selectedLetters);
+    this.updateResult();
   }
 
-  updateResult = () => {
-    let selection = this.selectedLetters.join('')
-    selection = selection.replace(/ +/g, '') // remove whitespace
-    console.log(selection, licensePlateData[selection])
+  updateResult() {
+    let selection = this.selectedLetters.join('');
+    selection = selection.replace(/ +/g, ''); // remove whitespace
+    console.log(selection, licensePlateData[selection]);
 
-    let cur = licensePlateData[selection]
+    let cur = licensePlateData[selection];
     if (cur === undefined) {
       this.setState({
-        result: `${selection}: undefined`
-      })
+        result: `${selection}: undefined`,
+      });
     } else {
       this.setState({
-        result: `${selection}: ${cur.city} (${cur.source}) -- ${cur.county}`
-      })
+        result: `${selection}: ${cur.city} (${cur.source}) -- ${cur.county}`,
+      });
     }
   }
 
   render() {
     return (
       <View style={styles.container}>
-      <View style={styles.intro}>
-        <Text>Welcome</Text>
-      </View>
-      <View style={styles.pickerContainer}>
-        <WheelPicker
-          style={styles.picker}
-          data={letterList}
-          onItemSelected={item => this.onItemSelected(item, 0)}/>
-        <WheelPicker
-          style={styles.picker}
-          data={letterList}
-          onItemSelected={item => this.onItemSelected(item, 1)}/>
-        <WheelPicker
-          style={styles.picker}
-          data={letterList}
-          onItemSelected={item => this.onItemSelected(item, 2)}/>
+        <View style={styles.intro}>
+          <Text>Welcome</Text>
         </View>
-      <View style={styles.results}>
-        <Text>{this.state.result}</Text>
-      </View>
+        <View style={styles.pickerContainer}>
+          <WheelPicker
+            style={styles.picker}
+            data={letterList}
+            onItemSelected={item => this.onItemSelected(item, 0)}
+          />
+          <WheelPicker
+            style={styles.picker}
+            data={letterList}
+            onItemSelected={item => this.onItemSelected(item, 1)}
+          />
+          <WheelPicker
+            style={styles.picker}
+            data={letterList}
+            onItemSelected={item => this.onItemSelected(item, 2)}
+          />
+        </View>
+        <View style={styles.results}>
+          <Text>{this.state.result}</Text>
+        </View>
       </View>
     );
   }
